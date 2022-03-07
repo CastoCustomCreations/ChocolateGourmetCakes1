531a1fe8a742ded5ff185d8b03bc2047386416d2
Jen Casto <CastoCustomCreations@gmail.com>
2022-03-07 10:49:16 -0500

    Add files via upload

---
+++ database/cgcdb.sql
@@ -0,0 +1,238 @@
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+SET time_zone = "+00:00";
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+
+DELIMITER $$
+DROP PROCEDURE IF EXISTS `getcat`$$
+CREATE DEFINER=`root`@`localhost` PROCEDURE `getcat` (IN `cid` INT)  SELECT * FROM categories WHERE cat_id=cid$$
+
+DELIMITER ;
+
+DROP TABLE IF EXISTS `admin_info`;
+CREATE TABLE IF NOT EXISTS `admin_info` (
+  `admin_id` int(10) NOT NULL AUTO_INCREMENT,
+  `admin_name` varchar(100) NOT NULL,
+  `admin_email` varchar(300) NOT NULL,
+  `admin_password` varchar(300) NOT NULL,
+  PRIMARY KEY (`admin_id`)
+) ENGINE=InnoDB AUTO_INCREMENT=2 DEFAULT CHARSET=latin1;
+
+INSERT INTO `admin_info` (`admin_id`, `admin_name`, `admin_email`, `admin_password`) VALUES
+(1, 'admin', 'admin@gmail.com', '25f9e794323b453885f5181f1b624d0b');
+
+DROP TABLE IF EXISTS `brands`;
+CREATE TABLE IF NOT EXISTS `brands` (
+  `brand_id` int(100) NOT NULL AUTO_INCREMENT,
+  `brand_title` text NOT NULL,
+  PRIMARY KEY (`brand_id`)
+) ENGINE=InnoDB AUTO_INCREMENT=7 DEFAULT CHARSET=latin1;
+
+INSERT INTO `brands` (`brand_id`, `brand_title`) VALUES
+(1, 'Homemade'),
+(2, 'Store bought');
+
+DROP TABLE IF EXISTS `cart`;
+CREATE TABLE IF NOT EXISTS `cart` (
+  `id` int(10) NOT NULL AUTO_INCREMENT,
+  `p_id` int(10) NOT NULL,
+  `ip_add` varchar(250) NOT NULL,
+  `user_id` int(10) DEFAULT NULL,
+  `qty` int(10) NOT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=72 DEFAULT CHARSET=latin1;
+
+INSERT INTO `cart` (`id`, `p_id`, `ip_add`, `user_id`, `qty`) VALUES
+(6, 26, '::1', 4, 1),
+(9, 10, '::1', 7, 1),
+(10, 11, '::1', 7, 1),
+(11, 45, '::1', 7, 1),
+(44, 5, '::1', 3, 0),
+(46, 2, '::1', 3, 0),
+(48, 72, '::1', 3, 0),
+(49, 60, '::1', 8, 1),
+(50, 61, '::1', 8, 1),
+(51, 1, '::1', 8, 1),
+(52, 5, '::1', 9, 1),
+(53, 2, '::1', 14, 1),
+(54, 3, '::1', 14, 1),
+(55, 5, '::1', 14, 1),
+(56, 1, '::1', 9, 1),
+(57, 2, '::1', 9, 1),
+(71, 61, '127.0.0.1', -1, 1);
+
+DROP TABLE IF EXISTS `categories`;
+CREATE TABLE IF NOT EXISTS `categories` (
+  `cat_id` int(100) NOT NULL AUTO_INCREMENT,
+  `cat_title` text NOT NULL,
+  PRIMARY KEY (`cat_id`)
+) ENGINE=InnoDB AUTO_INCREMENT=8 DEFAULT CHARSET=latin1;
+
+INSERT INTO `categories` (`cat_id`, `cat_title`) VALUES
+(1, 'Brownie'),
+(2, 'Cake'),
+(3, 'Cheesecake'),
+(4, 'Cookie'),
+(5, 'Cupcake'),
+(6, 'Muffin');
+
+DROP TABLE IF EXISTS `email_info`;
+CREATE TABLE IF NOT EXISTS `email_info` (
+  `email_id` int(100) NOT NULL AUTO_INCREMENT,
+  `email` text NOT NULL,
+  PRIMARY KEY (`email_id`)
+) ENGINE=InnoDB AUTO_INCREMENT=6 DEFAULT CHARSET=latin1;
+
+INSERT INTO `email_info` (`email_id`, `email`) VALUES
+(3, 'admin@gmail.com'),
+(4, 'puneethreddy951@gmail.com'),
+(5, 'puneethreddy@gmail.com');
+
+DROP TABLE IF EXISTS `logs`;
+CREATE TABLE IF NOT EXISTS `logs` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `user_id` varchar(50) NOT NULL,
+  `action` varchar(50) NOT NULL,
+  `date` datetime NOT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+DROP TABLE IF EXISTS `orders`;
+CREATE TABLE IF NOT EXISTS `orders` (
+  `order_id` int(11) NOT NULL AUTO_INCREMENT,
+  `user_id` int(11) NOT NULL,
+  `product_id` int(11) NOT NULL,
+  `qty` int(11) NOT NULL,
+  `trx_id` varchar(255) NOT NULL,
+  `p_status` varchar(20) NOT NULL,
+  PRIMARY KEY (`order_id`)
+) ENGINE=InnoDB AUTO_INCREMENT=3 DEFAULT CHARSET=latin1;
+
+DROP TABLE IF EXISTS `orders_info`;
+CREATE TABLE IF NOT EXISTS `orders_info` (
+  `order_id` int(10) NOT NULL AUTO_INCREMENT,
+  `user_id` int(11) NOT NULL,
+  `f_name` varchar(255) NOT NULL,
+  `email` varchar(255) NOT NULL,
+  `address` varchar(255) NOT NULL,
+  `city` varchar(255) NOT NULL,
+  `state` varchar(255) NOT NULL,
+  `zip` int(10) NOT NULL,
+  `cardname` varchar(255) NOT NULL,
+  `cardnumber` varchar(20) NOT NULL,
+  `expdate` varchar(255) NOT NULL,
+  `prod_count` int(15) DEFAULT NULL,
+  `total_amt` int(15) DEFAULT NULL,
+  `cvv` int(5) NOT NULL,
+  PRIMARY KEY (`order_id`),
+  KEY `user_id` (`user_id`)
+) ENGINE=InnoDB AUTO_INCREMENT=2 DEFAULT CHARSET=latin1;
+
+INSERT INTO `orders_info` (`order_id`, `user_id`, `f_name`, `email`, `address`, `city`, `state`, `zip`, `cardname`, `cardnumber`, `expdate`, `prod_count`, `total_amt`, `cvv`) VALUES
+(1, 12, 'Puneeth', 'puneethreddy951@gmail.com', 'Bangalore, Kumbalagodu, Karnataka', 'Bangalore', 'Karnataka', 560074, 'pokjhgfcxc', '4321 2345 6788 7654', '12/90', 3, 77000, 1234);
+
+DROP TABLE IF EXISTS `order_products`;
+CREATE TABLE IF NOT EXISTS `order_products` (
+  `order_pro_id` int(10) NOT NULL AUTO_INCREMENT,
+  `order_id` int(11) NOT NULL,
+  `product_id` int(11) NOT NULL,
+  `qty` int(15) DEFAULT NULL,
+  `amt` int(15) DEFAULT NULL,
+  PRIMARY KEY (`order_pro_id`),
+  KEY `order_products` (`order_id`),
+  KEY `product_id` (`product_id`)
+) ENGINE=InnoDB AUTO_INCREMENT=76 DEFAULT CHARSET=latin1;
+
+DROP TABLE IF EXISTS `products`;
+CREATE TABLE IF NOT EXISTS `products` (
+  `product_id` int(100) NOT NULL AUTO_INCREMENT,
+  `product_cat` int(100) NOT NULL,
+  `product_brand` int(100) NOT NULL,
+  `product_title` varchar(255) NOT NULL,
+  `product_price` int(100) NOT NULL,
+  `product_desc` text NOT NULL,
+  `product_image` text NOT NULL,
+  `product_keywords` text NOT NULL,
+  PRIMARY KEY (`product_id`)
+) ENGINE=InnoDB AUTO_INCREMENT=82 DEFAULT CHARSET=latin1;
+
+INSERT INTO `products` (`product_id`, `product_cat`, `product_brand`, `product_title`, `product_price`, `product_desc`, `product_image`, `product_keywords`) VALUES
+(1, 2, 1, 'Black Magic Cake', 35, '10\" Dark Chocolate Espresso cake with Marshmallow frosting', '1638563137_Black Magic Cake.png', 'Dark Chocolate, Espresso, Cake, Marshmallow '),
+(2, 2, 1, 'Buttered Pecan Fruitcake', 50, '10\" round', '1634324375_Buttered Pecan Fruitcake.png', 'Buttered Pecan Fruitcake cake'),
+(3, 1, 1, 'Dark Chocolate Brownie', 20, '9 - pack', '1638560508_DarkChocBrownie.png', 'Dark Chocolate Brownie'),
+(4, 1, 1, 'Semi-Sweet Fudge Brownie', 20, '9 - pack', '1638560572_SemiSweetFudgeBrownie.png', 'Semi-Sweet Fudge Brownie'),
+(5, 1, 1, 'Turtle Brownie', 20, '9 - pack', '1638560626_TurtleBrownie.png', 'chocolate pecan caramel brownie'),
+(6, 3, 1, 'Caramel Pretzel Cheesecake', 60, '10\" round', '1634319772_Caramel Pretzel Cheesecake.png', 'Caramel Pretzel Cheesecake'),
+(7, 3, 1, 'Oreo Cheesecake', 60, '10\" round', '1638561996_1634322663_Oreo Cheesecake.png', 'Oreo Cheesecake'),
+(8, 4, 1, 'Chocolate Chip Cookies', 26, '12 large cookies', '1638561615_ChocChipCookies.png', 'Chocolate Chip Cookies'),
+(9, 4, 1, 'Peanut Butter Cookies', 26, '12 large cookies', '1638561530_PeanutButterCookies.png', 'Peanut Butter Cookies'),
+(10, 4, 1, 'Trilbie', 30, '6 date filled sandwich cookies', '1638561342_Trilby.png', 'date sandwich cookie'),
+(11, 5, 1, 'Vanilla Sprinkle Cupcake', 20, '6 - pack', '1638557863_VanillaSprinleCupcake.png', 'Vanilla Sprinkle Cupcake'),
+(12, 5, 1, 'Oreo Cupcake', 20, '6 - pack', '1638558819_OreoCupcake.png', 'Oreo Cupcake'),
+(13, 6, 1, 'Blueberry muffin', 16, '6 - pack', '1638559371_BlueberryMuffin.png', 'blueberry muffin'),
+(14, 6, 1, 'Triple Berry Muffin', 16, '6 - pack', '1638558885_TripleBerryMuffin.png', 'berry muffin');
+
+DROP TABLE IF EXISTS `user_info`;
+CREATE TABLE IF NOT EXISTS `user_info` (
+  `user_id` int(10) NOT NULL AUTO_INCREMENT,
+  `first_name` varchar(100) NOT NULL,
+  `last_name` varchar(100) NOT NULL,
+  `email` varchar(300) NOT NULL,
+  `password` varchar(300) NOT NULL,
+  `mobile` varchar(10) NOT NULL,
+  `address1` varchar(300) NOT NULL,
+  `address2` varchar(11) NOT NULL,
+  PRIMARY KEY (`user_id`)
+) ENGINE=InnoDB AUTO_INCREMENT=26 DEFAULT CHARSET=latin1;
+
+INSERT INTO `user_info` (`user_id`, `first_name`, `last_name`, `email`, `password`, `mobile`, `address1`, `address2`) VALUES
+(12, 'Jen', 'Casto\r\n', 'castocustomcreations@gmail.com', 'Booksarefun1', '1234567898', '123456789', 'asdfghjkl');
+DROP TRIGGER IF EXISTS `after_user_info_insert`;
+DELIMITER $$
+CREATE TRIGGER `after_user_info_insert` AFTER INSERT ON `user_info` FOR EACH ROW BEGIN 
+INSERT INTO user_info_backup VALUES(new.user_id,new.first_name,new.last_name,new.email,new.password,new.mobile,new.address1,new.address2);
+END
+$$
+DELIMITER ;
+
+DROP TABLE IF EXISTS `user_info_backup`;
+CREATE TABLE IF NOT EXISTS `user_info_backup` (
+  `user_id` int(10) NOT NULL AUTO_INCREMENT,
+  `first_name` varchar(100) NOT NULL,
+  `last_name` varchar(100) NOT NULL,
+  `email` varchar(300) NOT NULL,
+  `password` varchar(300) NOT NULL,
+  `mobile` varchar(10) NOT NULL,
+  `address1` varchar(300) NOT NULL,
+  `address2` varchar(11) NOT NULL,
+  PRIMARY KEY (`user_id`)
+) ENGINE=InnoDB AUTO_INCREMENT=26 DEFAULT CHARSET=latin1;
+
+INSERT INTO `user_info_backup` (`user_id`, `first_name`, `last_name`, `email`, `password`, `mobile`, `address1`, `address2`) VALUES
+(12, 'puneeth', 'Reddy', 'puneethreddy951@gmail.com', '123456789', '9448121558', '123456789', 'sdcjns,djc'),
+(14, 'hemanthu', 'reddy', 'hemanthreddy951@gmail.com', '123456788', '6526436723', 's,dc wfjvnvn', 'b efhfhvvbr'),
+(15, 'hemu', 'ajhgdg', 'keeru@gmail.com', '346778', '536487276', ',mdnbca', 'asdmhmhvbv'),
+(16, 'venky', 'vs', 'venkey@gmail.com', '1234534', '9877654334', 'snhdgvajfehyfygv', 'asdjbhfkeur'),
+(19, 'abhishek', 'bs', 'abhishekbs@gmail.com', 'asdcsdcc', '9871236534', 'bangalore', 'hassan'),
+(20, 'pramod', 'vh', 'pramod@gmail.com', '124335353', '9767645653', 'ksbdfcdf', 'sjrgrevgsib'),
+(21, 'prajval', 'mcta', 'prajvalmcta@gmail.com', '1234545662', '202-555-01', 'bangalore', 'kumbalagodu'),
+(22, 'puneeth', 'v', 'hemu@gmail.com', '1234534', '9877654334', 'snhdgvajfehyfygv', 'asdjbhfkeur'),
+(23, 'hemanth', 'reddy', 'hemanth@gmail.com', 'Puneeth@123', '9876543234', 'Bangalore', 'Kumbalagodu'),
+(24, 'newuser', 'user', 'newuser@gmail.com', 'puneeth@123', '9535688928', 'Bangalore', 'Kumbalagodu'),
+(25, 'otheruser', 'user', 'otheruser@gmail.com', 'puneeth@123', '9535688928', 'Bangalore', 'Kumbalagodu');
+
+
+ALTER TABLE `orders_info`
+  ADD CONSTRAINT `user_id` FOREIGN KEY (`user_id`) REFERENCES `user_info` (`user_id`);
+
+ALTER TABLE `order_products`
+  ADD CONSTRAINT `order_products` FOREIGN KEY (`order_id`) REFERENCES `orders_info` (`order_id`) ON DELETE NO ACTION ON UPDATE CASCADE,
+  ADD CONSTRAINT `product_id` FOREIGN KEY (`product_id`) REFERENCES `products` (`product_id`);
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
